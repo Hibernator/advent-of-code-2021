0c8e55b5e32af7722388145ebd36ff73967e3f41
Hibernator <gibernator@gmail.com>
2021-12-04 15:40:16 +0100

    Puzzle 1

---
+++ src/main/scala/ch/hibernator/adventofcode/Puzzle1.scala
@@ -0,0 +1,30 @@
+package ch.hibernator.adventofcode
+
+import scala.io.Source
+
+object Puzzle1 extends App {
+  private val source = Source.fromFile("input1.txt")
+  val input: Seq[String] = source.getLines().toSeq
+  source.close()
+
+  val depths = input.map(_.toInt)
+
+  val increases = depths.init
+    .zip(depths.tail)
+    .map { case (first, second) =>
+      second > first
+    }
+    .count(_ == true)
+
+  println(increases)
+
+  val windowSums = depths.sliding(3).map(_.sum).toSeq
+  val windowIncreases = windowSums.init
+    .zip(windowSums.tail)
+    .map { case (first, second) =>
+      second > first
+    }
+    .count(_ == true)
+
+  println(windowIncreases)
+}
